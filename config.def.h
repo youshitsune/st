de957477525bf703f9621ac0d3594e0f24996999
Luka Jovanovic <youshitsune@protonmail.com>
2022-06-15 12:48:49 +0200

    Changed font

--- config.def.h
+++ config.def.h
@@ -5,8 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = {"JoyPixels:pixelsize=10:antialias=true:autohint=true"};
+static char *font = "Terminus:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
